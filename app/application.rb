9f2b8aadf7a6f6bf94fca87500107228b81c43f6
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-04-16 21:47:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,16 +12,17 @@
 # end
 
 class Application
-  @@items = ["Burger", "Quest Bar", "Protein Shake"]
-  
+ 
+  @@items = ["Apples","Carrots","Pears"]
+ 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    
+ 
     @@items.each do |item|
-      resp.write"#{item} \n"
+      resp.write "#{item}\n"
     end
-    
+ 
     resp.finish
   end
 end
